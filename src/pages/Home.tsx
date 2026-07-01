8b6ee9426f392df0163c13741e74276ff3104800
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-07-01 17:47:02 +0500

    initial scaffold

---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-900 text-white">
+      <p>Loading Blue Tic Tac Toe...</p>
+    </div>
+  );
+}
